2048fc686031f5ecacdbf14592d496f005c42905
HiCharly <caillon.charly@gmail.com>
2025-09-20 15:58:48 +0200

    feat: temporary disable dark mode

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -2,10 +2,17 @@
     @include('partials.settings-heading')
 
     <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
-        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
+        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance" disabled>
             <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
             <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
             <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
         </flux:radio.group>
+
+        <flux:callout icon="clock" class="mt-4">
+            <flux:callout.heading>Bientôt disponible</flux:callout.heading>
+            <flux:callout.text>
+                Le développement du thème sombre est en cours. Pour le moment, l'application utilise le thème clair par défaut.
+            </flux:callout.text>
+        </flux:callout>
     </x-settings.layout>
 </section>
