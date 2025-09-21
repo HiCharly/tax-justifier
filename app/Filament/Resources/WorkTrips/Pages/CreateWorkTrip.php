e5eec51dc024bd1d6e6ffa7a6a4d95c68ca12cdf
HiCharly <caillon.charly@gmail.com>
2025-09-21 16:10:57 +0200

    refactor: pint

--- app/Filament/Resources/WorkTrips/Pages/CreateWorkTrip.php
+++ app/Filament/Resources/WorkTrips/Pages/CreateWorkTrip.php
@@ -12,6 +12,7 @@ class CreateWorkTrip extends CreateRecord
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['user_id'] = auth()->id();
+
         return $data;
     }
 }
