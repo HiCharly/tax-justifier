df86b5029af3252c63333b8799f8da8db038cffc
HiCharly <caillon.charly@gmail.com>
2025-09-24 19:24:08 +0200

    feat: add password reset and email verification features with French translations

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -29,6 +29,8 @@ class AppPanelProvider extends PanelProvider
             ->id('app')
             ->path('app')
             ->login()
+            ->registration()
+            ->passwordReset()
             ->colors([
                 'primary' => Color::Indigo,
             ])
