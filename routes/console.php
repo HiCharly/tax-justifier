2ec0b6233c0c88726209e59b426817147dbeca50
HiCharly <caillon.charly@gmail.com>
2025-09-24 18:51:43 +0200

    feat: add weekly notification command for work trips and email template

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Console\Commands\SendWeeklyWorkTripsNotification;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(SendWeeklyWorkTripsNotification::class)
+    ->weeklyOn(5, '18:00');
